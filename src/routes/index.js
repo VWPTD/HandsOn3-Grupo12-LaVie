afae2e21a65930e407be2268a71c5c9dce2983a9
Ivan Yura Takano <ivanyurat@gmail.com>
2022-05-05 18:10:46 -0300

    criacao dos crud de atendimento

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const loginController = require("../controllers/loginController");
 const PacienteController = require("../controllers/pacienteController");
 const PsicologoController = require("../controllers/psicologoController");
+const AtendimentoController = require("../controllers/atendimentoController");
 const cadastroPsicologoValidator = require("../validators/cadastroPsicologoValidator");
 const cadastroPacienteValidador = require("../validators/cadastroPacienteValidador");
 const loginValidator = require("../validators/loginValidator");
@@ -17,7 +18,10 @@ routes.put("/psicologo/:id/atualizar", PsicologoController.atualizarPsicologo);
 routes.post("/psicologo/cadastrar", cadastroPsicologoValidator, PsicologoController.cadastrarPsicologo);
 routes.delete("/psicologo/:id/deletar", PsicologoController.deletarPsicologo);
 
-routes.post("/login", loginValidator, loginController.login)
+routes.post("/login", loginValidator, loginController.login);
+
+routes.get("/atendimento", AtendimentoController.listarAtendimento);
+routes.post("/atendimento/cadastrar", AtendimentoController.cadastrarAtendimento);
 
 
 module.exports = routes;
