b05dcb8aede30284ba6e9bd8c1422b1639dccc93
Isabela <isabela.ribeiro@outlook.com>
2022-05-06 10:24:20 -0300

    integrando o not null nas tabelas

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const loginController = require("../controllers/loginController");
+const PacienteController = require("../controllers/pacienteController");
+const PsicologoController = require("../controllers/psicologoController");
+const cadastroPsicologoValidator = require("../validators/cadastroPsicologoValidator");
+const loginValidator = require("../validators/loginValidator");
+const routes = express.Router();
+
+routes.get("/paciente", PacienteController.listarPaciente);
+routes.put("/paciente/:id/atualizar", PacienteController.atualizarPaciente);
+routes.post("/paciente/cadastrar", PacienteController.cadastrarPaciente);
+routes.delete("/paciente/:id/deletar", PacienteController.deletarPaciente);
+
+routes.get("/psicologo", PsicologoController.listarPsicologo);
+routes.put("/psicologo/:id/atualizar", PsicologoController.atualizarPsicologo);
+routes.post("/psicologo/cadastrar", cadastroPsicologoValidator, PsicologoController.cadastrarPsicologo);
+routes.delete("/psicologo/:id/deletar", PsicologoController.deletarPsicologo);
+
+routes.post("/login", loginValidator, loginController.login)
+
+
+module.exports = routes;
